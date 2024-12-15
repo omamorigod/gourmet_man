b1276e8a6c4654978f1fb815bd0791a08ea4f242
omamorigod <morimura33@gmail.com>
2024-12-15 16:38:46 +0900

    Update main.js

--- scr/main.js
+++ scr/main.js
@@ -5,13 +5,14 @@ function createGourmet_man( count ){
     const imagecontents = document.getElementById('imagecontents');
     for(i=1;i<=count;i++){
     let borderType = document.createElement('p');
-    borderType.style.borderBottom = "1px solid red";
+
     let img_element = document.createElement('img');
     img_element.src = `image/${i}.jpg`; 
     img_element.className  = ('imagecontents');
     borderType.appendChild(img_element);
 
     imagecontents.appendChild(borderType);
+    borderType.style.borderBottom = "1px solid red";
     };
 };
 

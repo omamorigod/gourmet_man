15b55de2af428e85a68aa0978d18d68b22f14092
omamorigod <morimura33@gmail.com>
2024-12-15 14:48:12 +0900

    Update new.js
    本当はイメージの数を取得したい

--- scr/new.js
+++ scr/new.js
@@ -1,11 +1,11 @@
-function CreateGourmet_man(count){
+const num = 30;
+function CreateOneGourmet_man(count){
     const imagecontents = document.getElementById('newimagecontent');
-    for(i=1;i<=count;i++){
     let img_element = document.createElement('img');
-    img_element.src = `image/${i}.jpg`; 
+    img_element.src = `image/${count}.jpg`; 
     imagecontents.appendChild(img_element);
     };
 };
 
 
-CreateGourmet_man(1);
+CreateOneGourmet_man(num);

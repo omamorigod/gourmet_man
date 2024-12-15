df5306e08c2e405a9e8f37029ff97054c37bd3cc
omamorigod <morimura33@gmail.com>
2024-12-15 15:24:52 +0900

    Update main.js
    async　thenでも表示が遅れているから

--- scr/main.js
+++ scr/main.js
@@ -1,30 +1,45 @@
+
 const num = 30;
 
-function createGourmet_man(count){
+function createGourmet_man( count ){
     const imagecontents = document.getElementById('imagecontents');
-    for(i=1;i<=count;i++){
+    for(imageNum=0;imageNum<=count;imageNum++){
     let img_element = document.createElement('img');
-    img_element.src = `image/${i}.jpg`; 
+    img_element.src = `image/${imageNum+1}.jpg`; 
     img_element.className  = ('imagecontents');
     imagecontents.appendChild(img_element);
+    createBorder(imageNum)
     };
 };
 
-function createBorder(){
+// function createBorder(){
+//     let imagecontent = document.getElementsByClassName('imagecontents');
+//     for(imageNum=0;imageNum<imagecontent.length;imageNum++){
+//         let border = document.createElement('p');
+//         border.className  = ('border');
+//         imagecontent[imageNum].after(border);
+//     };
+// };
+
+
+function createBorder(imageNum){
     let imagecontent = document.getElementsByClassName('imagecontents');
-    for(imageNum=0;imageNum<imagecontent.length;imageNum++){
+    // for(imageNum=0;imageNum<imagecontent.length;imageNum++){
         let border = document.createElement('p');
         border.className  = ('border');
         imagecontent[imageNum].after(border);
-    };
+    // };
 };
 
 
-const getImageContents = async () => {
-    createGourmet_man(num);
-    return true;
-}
 
-getImageContents().then(createBorder());
+// const getImageContents = async () => {
+//     createGourmet_man(num);
+//     return true;
+// }
+
+// getImageContents().then(createBorder());
+
+createGourmet_man(num);
 
 

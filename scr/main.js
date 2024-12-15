cdb03f7eca9d9ec4b292cb138af04e0c0ffc1cb3
omamorigod <morimura33@gmail.com>
2024-12-15 16:35:52 +0900

    Update main.js

--- scr/main.js
+++ scr/main.js
@@ -1,28 +1,18 @@
 const num = 30;
+const num = 30;
 
 function createGourmet_man( count ){
     const imagecontents = document.getElementById('imagecontents');
     for(i=1;i<=count;i++){
+    let borderType = document.createElement('p');
+    borderType.style.borderBottom = "1px solid red";
     let img_element = document.createElement('img');
     img_element.src = `image/${i}.jpg`; 
     img_element.className  = ('imagecontents');
-    imagecontents.appendChild(img_element);
+    borderType.appendChild(img_element);
+
+    imagecontents.appendChild(borderType);
     };
 };
 
-function createBorder(){
-    let imagecontent = document.getElementsByClassName('imagecontents');
-    for(imageNum=0;imageNum<imagecontent.length;imageNum++){
-        let borderType = document.createElement('p');
-        borderType.style.border = "1px solid red";
-        imagecontent[imageNum].after(borderType);
-    };
-};
-
-
-const getImageContents = async () => {
-    let result = await createGourmet_man(num);
-    return result;
-}
-
-getImageContents().then(createBorder());
+createGourmet_man(num)
